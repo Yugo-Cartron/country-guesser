3077e467e79d49a9f6fa0ea030de3b92167ba176
Yugo-Cartron <yugo.cartron@gmail.com>
2023-07-07 17:14:32 +0200

    :recycle: & :lipstick: : Add Tagline component and improve UI

--- src/components/TagHistory.vue
+++ src/components/TagHistory.vue
@@ -3,41 +3,22 @@ import {ref} from "vue";
 import arrow from "../assets/arrow.svg"
 
 const props = defineProps({
-  isCountryName: Boolean,
   label: String,
-  direction: Number,
+  rotation: String,
   good: Boolean,
 })
+const isArrow = ref(true)
+isArrow.value = !props.good
 
 const backgroundColor=ref('#CC0033')
 backgroundColor.value =
     props.good ? "#009934" : "#CC0033";
 
-const arrowDirection = ref('rotate-0')
-const isArrow = ref(false)
 const tagLabel = ref(props.label)
 
-tagLabel.value = canBeCastToNumber(props.label)
-  ? enhanceBigNumbers(props.label)
+tagLabel.value = canBeCastToNumber(props.label!)
+  ? enhanceBigNumbers(props.label!)
   : props.label
-
-if( props.direction < 4 ) {
-  isArrow.value = true
-  switch (props.direction) {
-    case 1:
-      arrowDirection.value = "rotate-90"
-      break
-    case 2:
-      arrowDirection.value = "rotate-180"
-      break
-    case 3:
-      arrowDirection.value = "-rotate-90"
-      break
-    default :
-      arrowDirection.value = "rotate-0"
-      break
-  }
-}
 
 function canBeCastToNumber(str: string): boolean {
   return !isNaN(+str);
@@ -60,22 +41,13 @@ function enhanceBigNumbers(bigNumber: string) {
 </script>
 
 <template>
-  <div v-if="props.isCountryName">
-    <h3 class="flex justify-center text-lg mx-10 py-7">{{ props.label }}</h3>
-  </div>
-  <div v-else :class="`tagHistory flex flex-row w-fit h-fit mx-auto my-auto py-0.5 px-6 gap-2 justify-center items-center bg-[${backgroundColor}]`">
-    <img v-if="isArrow" :class="`w-fit h-fit ${arrowDirection}`" :src="arrow" />
-    <p class="text-white text-xl">{{ tagLabel }}</p>
+  <div>
+    <div :class="`rounded-full flex flex-row w-fit h-fit mx-auto my-auto py-0.5 px-6 gap-2 justify-center items-center bg-[${backgroundColor}]`">
+      <img v-if="isArrow" :class="`w-fit h-fit ${ props.rotation }`" :src="arrow" />
+      <p class="text-white text-xl">{{ tagLabel }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
-.tagHistory {
- font-size: 1rem;
- opacity: 1;
- transition: opacity 1s ease;
-}
-
-
 </style>
---
+++ src/components/TagName.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String
+})
+
+</script>
+
+<template>
+    <h3 class="flex justify-center text-xl py-7">{{ props.name }}</h3>
+</template>
+
+<style scoped>
+
+</style>
