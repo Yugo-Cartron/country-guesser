daad5ddbf72dfe4c795b1976fcd0301acfa3615c
Yugo-Cartron <yugo.cartron@gmail.com>
2023-07-13 16:18:51 +0200

    fix autocomplete on prod

--- src/components/Suggestion.vue
+++ src/components/Suggestion.vue
@@ -18,7 +18,7 @@ function emitSelectedCountry() {
 
 <template>
 
-  <div class="flex flex-row items-center hover:bg-[#BF8055] hover:text-white" @click="emitSelectedCountry">
+  <div class="flex flex-row items-center hover:bg-[#BF8055] hover:text-white">
     <div class="w-16 m-4">
       <img :src="props.flag" />
     </div>
